16d3ae30712ce1f3c524bebc2298e034d93044b7
Sammy Kordi <sammy.kordi@gmail.com>
2024-09-23 16:31:33 -0700

    Updated functions

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav className="nav">
+      <ul>
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Candidate Search
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/SavedCandidates" className="nav-link">
+            Your Saved Candidates
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
+
